71d755e8f910e5e825d1448fc473d78e2c5ef5c3
Leonardo Sanchez <leonardojaviersanchez92@gmail.com>
2025-06-26 22:36:53 -0300

    ejercicio3

---
+++ ejercicio3/scripts.js
@@ -0,0 +1,14 @@
+const lista = document.getElementById("lista")
+const datos = fetch("https://jsonplaceholder.typicode.com/todos").then((res) => 
+    res.json()
+)
+
+datos.then((data) => {
+    data.filter((tarea) => {
+        if (tarea.completed) {
+            const li = document.createElement("li")
+            li.textContent = tarea.title
+            lista.appendChild(li)
+        }
+    })
+})
